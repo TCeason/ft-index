e844af90225e23a9a187f12c508e88a0e0938b3f
Rich Prohaska <prohaska@tokutek.com>
2014-07-25 14:46:28 -0400

    verify after checkpoint

--- src/tests/prune-empty-basements.cc
+++ src/tests/prune-empty-basements.cc
@@ -88,8 +88,20 @@ PATENT RIGHTS GRANT:
 
 #ident "Copyright (c) 2007-2013 Tokutek Inc.  All rights reserved."
 #ident "The technology is licensed by the Massachusetts Institute of Technology, Rutgers State University of New Jersey, and the Research Foundation of State University of New York at Stony Brook under United States of America Serial No. 11/760379 and to the patents and/or patent applications resulting from it."
+
+// Generate a tree, do some left side deletions, and verify that all of the expected rows are present.
+// A side effect is that basement nodes should be pruned, but there is no automated way to verify that.
+
 #include "test.h"
 #include <endian.h>
+#ifndef htobe63
+#include <byteswap.h>
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+#define htobe64(x) __bswap_64(x)
+#else
+#define htobe63(x) (x)
+#endif
+#endif
 
 static void populate(DB *db, DB_ENV *env, uint64_t n) {
     int r;
@@ -99,6 +111,7 @@ static void populate(DB *db, DB_ENV *env, uint64_t n) {
     CKERR(r);
 
     for (uint64_t i = 0; i < n; i++) {
+
         uint64_t k = htobe64(i);
         DBT key = { .data = &k, .size = sizeof k };
         DBT val = { .data = &i, .size = sizeof i};
@@ -135,7 +148,7 @@ static void checkpoint(DB_ENV *env) {
     CKERR(r);
 }
 
-static void run_test(void) {
+static void populate_and_delete(uint64_t ninsert, uint64_t ndelete) {
     int r;
 
     // create the env
@@ -154,20 +167,73 @@ static void run_test(void) {
     r = db->open(db, nullptr, "foo.db", 0, DB_BTREE, DB_CREATE, S_IRWXU+S_IRWXG+S_IRWXO);
     CKERR(r);
 
-    populate(db, env, 1000000);
+    populate(db, env, ninsert);
 
     // optimize(db, env);
 
     checkpoint(env);
 
-    deletesome(db, env, 110000);
+    deletesome(db, env, ndelete);
 
     checkpoint(env);
 
     r = db->close(db, 0);
     CKERR(r);
 
-    r = env->close(env, TOKUFT_DIRTY_SHUTDOWN);
+    r = env->close(env, 0);
+    CKERR(r);
+}
+
+static void verify(uint64_t ninsert, uint64_t ndelete) {
+    int r;
+
+    // create the env
+    DB_ENV *env = nullptr;
+    r = db_env_create(&env, 0);
+    CKERR(r);
+    r = env->open(env, TOKU_TEST_FILENAME, 
+                  DB_INIT_MPOOL|DB_CREATE|DB_THREAD |DB_INIT_LOCK|DB_INIT_LOG|DB_INIT_TXN|DB_PRIVATE, 
+                  S_IRWXU+S_IRWXG+S_IRWXO);
+    CKERR(r);
+
+    // create the db
+    DB *db = nullptr;
+    r = db_create(&db, env, 0);
+    CKERR(r);
+    r = db->open(db, nullptr, "foo.db", 0, DB_BTREE, 0, S_IRWXU+S_IRWXG+S_IRWXO);
+    CKERR(r);
+
+    DB_TXN *txn = nullptr;
+    r = env->txn_begin(env, nullptr, &txn, 0);
+    CKERR(r);
+
+    DBC *cursor = nullptr;
+    r = db->cursor(db, txn, &cursor, 0);
+    CKERR(r);
+
+    while (1) {
+        uint64_t k, v;
+        DBT key = {}; key.data = &k; key.ulen = sizeof k; key.flags = DB_DBT_USERMEM;
+        DBT val = {}; val.data = &v; val.ulen = sizeof v; val.flags = DB_DBT_USERMEM;
+        r = cursor->c_get(cursor, &key, &val, DB_NEXT);
+        if (r == DB_NOTFOUND)
+            break;
+        assert(key.size == key.ulen && val.size == val.ulen);
+        assert(htobe64(k) == ndelete && v == ndelete);
+        ndelete++;
+    }
+    assert(ndelete == ninsert);
+
+    r = cursor->c_close(cursor);
+    CKERR(r);
+    
+    r = txn->commit(txn, 0);
+    CKERR(r);
+
+    r = db->close(db, 0);
+    CKERR(r);
+
+    r = env->close(env, 0);
     CKERR(r);
 }
 
@@ -180,7 +246,8 @@ int test_main (int argc, char *const argv[]) {
     CKERR(r);
 
     // run the test
-    run_test();
+    populate_and_delete(1000000, 110000);
+    verify(1000000, 110000);
 
     return 0;
 }
